20d51f1935c88958a3dc7b6f6ae6d4557e4bb5b0
Rafael Albuquerque <rafae.albuquerque.it@gmail.com>
2021-07-14 14:43:10 -0300

    Project Ocean

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const lista = [
+  {
+    id: 0,
+    nome: "Golden Retriever",
+    imageUrl:
+      "https://static.wixstatic.com/media/db516d_a7f7619f878f4c29948251d50d7f8de9~mv2.jpg/v1/fill/w_569,h_512,al_c,lg_1,q_80,usm_1.20_1.00_0.01/db516d_a7f7619f878f4c29948251d50d7f8de9~mv2.webp",
+  },
+  {
+    id: 1,
+    nome: "Labrador",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAY-2Y-wO9bl9AibZsnHTcJz_Wi5tzkKuKKqZaXno8xy6SS2A2JQYeMOwshFSfZ1DNPBY&usqp=CAU",
+  },
+];
+
+function Item(props) {
+  const indice = props.indice;
+  const item = lista[indice];
+
+  return (
+    <div>
+      {item.nome}
+      <br />
+      <img src={item.imageUrl} alt={item.nome} width="200"></img>
+    </div>
+  );
+}
+
+function Lista() {
+  return (
+    <div>
+      {lista.map((item, index) => (
+        <Item indice={index} key={index} />
+      ))}
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Lista />
+    </div>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
