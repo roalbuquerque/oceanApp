faceace6096315ae6016ddaea48ffb711b8ccdfa
Rafael Albuquerque <rafae.albuquerque.it@gmail.com>
2021-07-20 17:05:40 -0300

    Prettier config

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
 const lista = [
   {
     id: 0,
-    nome: "Golden Retriever",
+    nome: 'Golden Retriever',
     imageUrl:
-      "https://static.wixstatic.com/media/db516d_a7f7619f878f4c29948251d50d7f8de9~mv2.jpg/v1/fill/w_569,h_512,al_c,lg_1,q_80,usm_1.20_1.00_0.01/db516d_a7f7619f878f4c29948251d50d7f8de9~mv2.webp",
+      'https://static.wixstatic.com/media/db516d_a7f7619f878f4c29948251d50d7f8de9~mv2.jpg/v1/fill/w_569,h_512,al_c,lg_1,q_80,usm_1.20_1.00_0.01/db516d_a7f7619f878f4c29948251d50d7f8de9~mv2.webp',
   },
   {
     id: 1,
-    nome: "Labrador",
+    nome: 'Labrador',
     imageUrl:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAY-2Y-wO9bl9AibZsnHTcJz_Wi5tzkKuKKqZaXno8xy6SS2A2JQYeMOwshFSfZ1DNPBY&usqp=CAU",
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAY-2Y-wO9bl9AibZsnHTcJz_Wi5tzkKuKKqZaXno8xy6SS2A2JQYeMOwshFSfZ1DNPBY&usqp=CAU',
   },
 ];
 
@@ -25,7 +25,7 @@ function Item(props) {
     <div>
       {item.nome}
       <br />
-      <img src={item.imageUrl} alt={item.nome} width="200"></img>
+      <img src={item.imageUrl} alt={item.nome} width='200'></img>
     </div>
   );
 }
@@ -52,5 +52,5 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
