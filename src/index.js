12a0610d56cbad6415dbe920aaac47aaaf93360e
Rafael Albuquerque <rafae.albuquerque.it@gmail.com>
2021-07-21 15:11:20 -0300

    Style in the title of card item

--- src/index.js
+++ src/index.js
@@ -23,8 +23,7 @@ function Item(props) {
 
   return (
     <div className='item'>
-      {item.nome}
-      <br />
+      <h1 className='item__title'>{item.nome}</h1>
       <img src={item.imageUrl} alt={item.nome} width='200'></img>
     </div>
   );
