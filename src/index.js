587c17b08a5e305174cf2f950c3560802a7ba434
Rafael Albuquerque <rafae.albuquerque.it@gmail.com>
2021-07-23 10:20:29 -0300

    update

--- src/index.js
+++ src/index.js
@@ -3,34 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './index.css';
 
-const lista = [
-  {
-    id: 1,
-    nome: 'Pinscher',
-    imageUrl: 'https://pbs.twimg.com/media/Da3FmbSWkAEuwUJ.jpg',
-  },
-  {
-    id: 2,
-    nome: 'Golden Retriever',
-    imageUrl: 'https://f.i.uol.com.br/fotografia/2017/10/27/150912457859f369e28eaa5_1509124578_3x2_md.jpg',
-  },
-  {
-    id: 3,
-    nome: 'Labrador',
-    imageUrl: 'https://www.clubeparacachorros.com.br/wp-content/uploads/2014/07/labrador-amarelo.jpg',
-  },
-  {
-    id: 4,
-    nome: 'Yorkshire',
-    imageUrl: 'https://www.petlove.com.br/images/breeds/192471/profile/original/yorkshire-p.jpg?1532539683',
-  },
-  {
-    id: 5,
-    nome: 'Husky Siberiano',
-    imageUrl: 'https://www.petlove.com.br/images/breeds/193082/profile/original/husky_siberian-p.jpg?1532539123',
-  },
-];
-
 function Item(props) {
   const item = props.item;
 
